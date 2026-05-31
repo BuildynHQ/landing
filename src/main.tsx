2b7ed3e1988f6bd0da8575719880f974488fffca
ruch <pushkarmhatre424@gmail.com>
2026-06-01 01:49:19 +0530

    refactor: remove Contact, Footer, Hero, Philosophy, Process, Services, and Work sections for codebase cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./lib/analytics";
 import "./index.css";
 import App from "./App";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
